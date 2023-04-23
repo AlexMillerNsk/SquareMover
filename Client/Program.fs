f313271d1b0e4fffa61a58358ef4a2905611d483
Aleksandr <love.me.lnikov@gmail.com>
2023-04-23 11:23:46 +0700

    Add project files.

---
+++ Client/Program.fs
@@ -0,0 +1,80 @@
+﻿module Client 
+
+open Elmish
+open Feliz
+open Fable.Core
+open Elmish.React
+open Fable.Import
+open Browser
+
+type Model = {
+    isDragging: bool
+    x: float
+    y: float}
+
+type Msg = 
+    |HandleMouseDown of MouseEvent
+    |HandleMouseMove of MouseEvent
+    |HandleMouseUp of MouseEvent
+
+let init() = { isDragging = false; x = 0; y = 0 }, Cmd.none
+
+let handleMouseMove  (ev: MouseEvent) model =
+    if model.isDragging then
+        let deltaX = ev.clientX - model.x
+        let deltaY = ev.clientY - model.y
+        let square = document.getElementById("square")
+        let prevX = float (square.style.left.Replace("px", ""))
+        let prevY = float (square.style.top.Replace("px", ""))
+        square.style.left <- string (prevX + deltaX) + "px"
+        square.style.top <- string (prevY + deltaY) + "px"
+        {model with x = ev.clientX; y = ev.clientY}
+    else model
+
+let update (msg:Msg) (model:Model) =
+    match msg with
+    | HandleMouseDown be -> {model with isDragging = true; x = be.clientX; y = be.clientY}, Cmd.none                               
+    | HandleMouseUp be -> {model with isDragging = false}, Cmd.none
+    | HandleMouseMove be ->     if model.isDragging then
+                                    let deltaX = be.clientX - model.x
+                                    let deltaY = be.clientY - model.y
+                                    let square = document.getElementById("square")
+                                    let prevX = float (square.style.left.Replace("px", ""))
+                                    let prevY = float (square.style.top.Replace("px", ""))
+                                    square.style.left <- string (prevX + deltaX) + "px"
+                                    square.style.top <- string (prevY + deltaY) + "px"
+                                    {model with x = be.clientX; y = be.clientY}, Cmd.none
+                                else model, Cmd.none
+
+
+
+let div (classes: string list) (children: ReactElement list) =
+    Html.div [
+        prop.classes classes
+        prop.children children
+    ]
+
+let render model (dispatch: Msg -> unit) =
+    div [
+        prop.onMouseDown (fun _ -> dispatch HandleMouseDown)
+        prop.onMouseMove (fun _ -> dispatch HandleMouseDown )
+        prop.onMouseUp (fun _ -> dispatch HandleMouseUp ) 
+     ] 
+   
+    div [
+        prop.style [
+            "position", "absolute"
+            "width", "100px"
+            "height", "100px"
+            "background-color", "blue"
+            "left", "0px"
+            "top", "0px"
+        ]
+        prop.id "square"
+    ] []
+    
+   ]
+
+Program.mkProgram init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
---
+++ ElmRemoting/Program.fs
@@ -0,0 +1,32 @@
+﻿module Server
+
+open System
+open System.IO
+open Suave
+open Filters
+open Operators
+
+
+[<EntryPoint>]
+let main argv =
+
+    let app: WebPart =
+         choose [
+              GET >=> path "/" >=> Files.browseFileHome "index.html"    
+              GET >=> path "/test" >=> Successful.OK "Biden666"
+              GET >=> Files.browseHome
+              RequestErrors.NOT_FOUND "Page not found." 
+              POST >=> path "/hello" >=> Successful.OK "Hello POST"]
+
+
+    let portEnvVar = Environment.GetEnvironmentVariable "PORT"
+    let port = if String.IsNullOrEmpty portEnvVar then 8080 else (int)portEnvVar
+    let config = {
+        defaultConfig with
+            bindings = [HttpBinding.createSimple HTTP "127.0.0.1" port]
+            homeFolder = Some(Path.GetFullPath "./Public") }
+    startWebServer config app
+    0
+
+
+
